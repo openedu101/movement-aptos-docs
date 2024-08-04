4427db64dc30f53efd5ffb02f89409a9d3b4d598
jayden-dang <jayden@jaydendang.com>
2024-08-05 00:16:59 +0700

    feat: update what is movement

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Raki Finance</span>,
+  logo: <span>Movement with Aptos</span>,
   project: {
-    link: 'https://github.com/rakigeek',
+    link: 'https://github.com/openedu101/movement-aptos-docs',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/rakigeek',
+  docsRepositoryBase: 'https://github.com/openedu101/movement-aptos-docs',
   footer: {
-    text: 'Raki Finance Documentation',
+    text: 'Movement with Aptos Workshop',
   },
 }
 
